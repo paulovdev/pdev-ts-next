71e109951717e197f7454ce118eda9a9408b6a31
Paulo Pimentel <fudevil123@gmail.com>
2024-12-08 09:22:16 -0300

    removing animatepresence

--- src/app/home/header/page.tsx
+++ src/app/home/header/page.tsx
@@ -1,14 +1,13 @@
 "use client"
 
 import { useState } from 'react';
-import { AnimatePresence } from 'framer-motion';
 import { FaLinkedin, FaTwitter, FaGithub } from "react-icons/fa";
 import { VscVerifiedFilled } from "react-icons/vsc";
 import { IoIosArrowUp } from "react-icons/io";
 
 import "./homeHeader.scss";
 
-export default function  HomeHeader () {
+export default function HomeHeader() {
     const [showMoreAbout, setShowMoreAbout] = useState(false);
 
     return (
@@ -49,17 +48,15 @@ export default function  HomeHeader () {
                     Front-end developer & UX/UI design, specializing in creating immersive and intuitive user experiences, consistently pushing the boundaries of design innovation.
                 </h2>
 
-                <AnimatePresence>
-                    {showMoreAbout && (
-                        <div className="modal-more-about">
-                            <p>As we talked about, I'm a front-end developer passionate about music and games. In addition to creating amazing interfaces for the web, I also dedicate my free time to producing music and exploring the world of games.</p>
+                {showMoreAbout && (
+                    <div className="modal-more-about">
+                        <p>As we talked about, I'm a front-end developer passionate about music and games. In addition to creating amazing interfaces for the web, I also dedicate my free time to producing music and exploring the world of games.</p>
 
-                            <p>I love working on challenging projects and diving into long, engaging texts. My creativity and technical skills allow me to create unique and engaging digital experiences for users. I always seek to improve my skills and stay up to date with the latest trends in the world of web development.</p>
+                        <p>I love working on challenging projects and diving into long, engaging texts. My creativity and technical skills allow me to create unique and engaging digital experiences for users. I always seek to improve my skills and stay up to date with the latest trends in the world of web development.</p>
 
-                            <span>Paulo Vitor Pimentel dos Santos</span>
-                        </div>
-                    )}
-                </AnimatePresence>
+                        <span>Paulo Vitor Pimentel dos Santos</span>
+                    </div>
+                )}
 
                 <button onClick={() => setShowMoreAbout(!showMoreAbout)} className={!showMoreAbout ? "active" : ""}>
                     More about me <IoIosArrowUp />
