3ba9f7ddac10e9904c277be3283452a1f026b626
Paulo Pimentel <fudevil123@gmail.com>
2024-12-10 21:54:42 -0300

    adding internationalize en + pt-br 100%

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from "@/context/ThemeContext";
 import TopNav from "@/components/topNav/page";
 import Nav from "@/components/nav/page";
 
-import {setRequestLocale} from 'next-intl/server';
+import { setRequestLocale } from 'next-intl/server';
 
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
@@ -11,8 +11,6 @@ import { notFound } from 'next/navigation';
 import { routing } from '@/i18n/routing';
 
 import "./globals.scss";
-
-
 
 export const metadata: Metadata = {
   title: "paulovdev - portfolio",
@@ -24,21 +22,22 @@ export const metadata: Metadata = {
 
 export default async function LocaleLayout({
   children,
-  params: { locale }
+  params,
 }: {
   children: React.ReactNode;
   params: { locale: string };
 }) {
+  const { locale } =  await params; // Destructure locale from params here
+
   // Ensure that the incoming `locale` is valid 
   setRequestLocale(locale);
+
+  console.log(locale);
   if (!routing.locales.includes(locale as any)) {
     notFound();
   }
-
   // Providing all messages to the client
-  // side is the easiest way to get started
   const messages = await getMessages();
- 
 
   return (
     <html lang={locale}>
